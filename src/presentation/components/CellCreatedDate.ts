8b782fe469b4fba1567f8710d2d268bac6be2954
metagliabue <metagliabue@profile.es>
2025-11-16 15:54:06 +0100

    feat: show relative date

---
+++ src/presentation/components/CellCreatedDate.ts
@@ -0,0 +1,53 @@
+export class CellCreatedDate extends HTMLElement {
+  private relativeDate: string = '';
+
+  constructor(createdAt: string,) {
+    super();
+    this.relativeDate = this.formatToRelative(new Date(createdAt));
+    this.render();
+  }
+  /* Lifecycle hook: cuando el elemento se enserte en el DOM */
+  connectedCallback() {
+    this.render();
+  }
+
+  /* renderiza el shadow DOM con los estilos encapsulados */
+  private render(): void {
+    this.className = 'cell';
+    this.innerHTML = this.getTemplate();
+
+    /* const shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = this.getTemplate();
+    //TODO: agregar estilos
+    shadow.appendChild(this.getStyles()); */
+  }
+
+  private formatToRelative(date: Date): string {
+    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+
+    const intervals = [
+      { label: 'year', seconds: 31536000 },
+      { label: 'month', seconds: 2592000 },
+      { label: 'day', seconds: 86400 },
+      { label: 'hour', seconds: 3600 },
+      { label: 'minute', seconds: 60 },
+      { label: 'second', seconds: 1 },
+    ];
+
+    for (const interval of intervals) {
+      const value = Math.floor(seconds / interval.seconds);
+      if (value >= 1) {
+        return `${value} ${interval.label}${value !== 1 ? 's' : ''} ago`;
+      }
+    }
+    return "just now";
+  }
+
+  private getTemplate(): string {
+    return `
+      <p>${this.relativeDate}</p>
+    `;
+  }
+
+}
+customElements.define('cell-created-at', CellCreatedDate);
--- src/presentation/components/Grid.ts
+++ src/presentation/components/Grid.ts
@@ -15,11 +15,12 @@ export class Grid extends HTMLElement {
   private render(): void {
     this.className = 'section';
     this.innerHTML = `
-      <div class="fixed-grid has-4-cols">
+      <div class="fixed-grid has-5-cols">
         <div class="grid">
           <div class="cell is-col-span-2"><strong>Name</strong></div>
           <div class="cell"><strong>Contributors</strong></div>
           <div class="cell"><strong>Attachments</strong></div>
+          <div class="cell"><strong>Created</strong></div>
         </div>
       </div>
     `;
